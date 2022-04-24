3583e63e5aeabd57b5fdf217674784d3f233f2f5
Hyeonsol Kong <besyia0k0@cau.ac.kr>
2022-04-24 19:39:28 +0900

    format

--- colors.js
+++ colors.js
@@ -1,41 +1,37 @@
 var Body = {
   setColor: function (color) {
-    $('body').css('color', color);
+    $("body").css("color", color);
     //document.querySelector('body').style.color = color;
   },
   setBackgroundcolor: function (color) {
-    $('body').css('backgroundColor', color);
+    $("body").css("backgroundColor", color);
     //document.querySelector('body').style.backgroundColor = color;
-  }
-}
-var	Links = {
-  setColor: function (color)
-  {
-    $('a').css('color', color);
-    $('#active').css('color', 'mediumpurple');
+  },
+};
+var Links = {
+  setColor: function (color) {
+    $("a").css("color", color);
+    $("#active").css("color", "mediumpurple");
     // var	list = document.querySelectorAll('a');
     // for (var i of list)
     // if (i.getAttribute('id') == 'active')
     //   i.style.color = 'mediumpurple';
     // else
     //   i.style.color = color;
-  }
-}
+  },
+};
 
 function nightDayHandler(self) {
-  var target = document.querySelector('body');
-  if (self.value == 'night')
-  {
-    self.value = 'day';
-    Body.setBackgroundcolor('black');
-    Body.setColor('white');
-    Link.setColor('powderblue');
-  }
-  else
-  {
-    self.value = 'night';
-    Body.setBackgroundcolor('white');
-    Body.setColor('black');
-    Link.setColor('cornflowerblue');
+  var target = document.querySelector("body");
+  if (self.value == "night") {
+    self.value = "day";
+    Body.setBackgroundcolor("black");
+    Body.setColor("white");
+    Link.setColor("powderblue");
+  } else {
+    self.value = "night";
+    Body.setBackgroundcolor("white");
+    Body.setColor("black");
+    Link.setColor("cornflowerblue");
   }
 }
